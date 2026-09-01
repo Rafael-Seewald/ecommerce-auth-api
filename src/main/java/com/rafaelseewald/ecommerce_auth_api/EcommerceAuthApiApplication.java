93df74fac84cd022f77d3011779b29afb78b86b7
Rafael-Seewald <rafa.vini.seewald@gmail.com>
2026-08-31 22:42:27 -0300

    chore: setup inicial do projeto spring boot

---
+++ src/main/java/com/rafaelseewald/ecommerce_auth_api/EcommerceAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelseewald.ecommerce_auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelseewald/ecommerce_auth_api/EcommerceAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelseewald.ecommerce_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
